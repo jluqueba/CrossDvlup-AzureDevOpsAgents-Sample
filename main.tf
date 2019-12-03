cf935bb4145cb8b257e910bf047001cc37fc1f50
x284783 <x284783@es.produban.com>
2019-12-03 16:14:28 +0100

    Updated terraform sample script

---
+++ main.tf
@@ -0,0 +1,32 @@
+data "azurerm_resource_group" "rsg" {
+  name = "${var.resource_group_name}"
+}
+
+data "azurerm_storage_account" "sta" {
+  name                     = "${var.storage_account_name}"
+  resource_group_name      = "${data.azurerm_resource_group.rsg.name}"
+
+  depends_on = ["data.azurerm_resource_group.rsg"]
+}
+
+data "azurerm_storage_container" "stc" {
+  name                  = "${var.storage_container_name}"
+  resource_group_name   = "${data.azurerm_resource_group.rsg.name}"
+  storage_account_name  = "${data.azurerm_storage_account.sta.name}"
+  
+  depends_on = ["data.azurerm_resource_group.rsg",
+                "data.azurerm_storage_account.sta"]
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "my-awesome-content.zip"
+  resource_group_name    = "${data.azurerm_resource_group.rsg.name}"
+  storage_account_name   = "${data.azurerm_storage_account.sta.name}"
+  storage_container_name = "${data.azurerm_storage_container.stc.name}"
+  type                   = "Block"
+  source                 = "testfile.txt"
+
+  depends_on = ["data.azurerm_resource_group.rsg",
+                "data.azurerm_storage_account.sta", 
+                "data.azurerm_storage_container.stc"]
+}
