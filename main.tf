b25a7c25137bc489517c04c3bd2a0e6070401df3
x284783 <x284783@es.produban.com>
2019-12-04 15:33:54 +0100

    Changed blob name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_blob" "blob" {
-  name                   = "my-awesome-content.zip"
+  name                   = "testfile.txt"
   resource_group_name    = var.resource_group_name
   storage_account_name   = var.storage_account_name
   storage_container_name = var.storage_container_name
