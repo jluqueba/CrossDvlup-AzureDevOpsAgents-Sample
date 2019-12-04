6a0eade62fc252143f22ede561142e9a67703de9
x284783 <x284783@es.produban.com>
2019-12-04 15:12:03 +0100

    Delete string interpolation

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 data "azurerm_resource_group" "rsg" {
-  name = "${var.resource_group_name}"
+  name = var.resource_group_name
 }
 
 data "azurerm_storage_account" "sta" {
-  name                     = "${var.storage_account_name}"
-  resource_group_name      = "${data.azurerm_resource_group.rsg.name}"
+  name                     = var.storage_account_name
+  resource_group_name      = data.azurerm_resource_group.rsg.name
 
   depends_on = [data.azurerm_resource_group.rsg]
 }
 
 data "azurerm_storage_container" "stc" {
-  name                  = "${var.storage_container_name}"
-  resource_group_name   = "${data.azurerm_resource_group.rsg.name}"
-  storage_account_name  = "${data.azurerm_storage_account.sta.name}"
+  name                  = var.storage_container_name
+  resource_group_name   = data.azurerm_resource_group.rsg.name
+  storage_account_name  = data.azurerm_storage_account.sta.name
   
   depends_on = [data.azurerm_resource_group.rsg,
                 data.azurerm_storage_account.sta]
 }
 
-resource "azurerm_storage_blob" "example" {
+resource "azurerm_storage_blob" "blob" {
   name                   = "my-awesome-content.zip"
-  resource_group_name    = "${data.azurerm_resource_group.rsg.name}"
-  storage_account_name   = "${data.azurerm_storage_account.sta.name}"
-  storage_container_name = "${data.azurerm_storage_container.stc.name}"
+  resource_group_name    = data.azurerm_resource_group.rsg.name
+  storage_account_name   = data.azurerm_storage_account.sta.name
+  storage_container_name = data.azurerm_storage_container.stc.name
   type                   = "Block"
   source                 = "testfile.txt"
 
